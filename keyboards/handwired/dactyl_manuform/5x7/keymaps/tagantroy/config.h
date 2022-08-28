5c3387a9a67c070ac49b06d69fdbced524f732dd
Ivan Balaksha <tagantroy@gmail.com>
2022-08-28 14:17:05 +0100

    add my layout

--- keyboards/handwired/dactyl_manuform/5x7/keymaps/tagantroy/config.h
+++ keyboards/handwired/dactyl_manuform/5x7/keymaps/tagantroy/config.h
@@ -1,6 +1,5 @@
 /*
 Copyright 2012 Jun Wako <wakojun@gmail.com>
-Copyright 2015 Jack Humbert
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -18,17 +17,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#include "config_common.h"
+#define USE_SERIAL
 
-/* key matrix size */
-// Rows are doubled-up
-#define MATRIX_ROWS 12
-#define MATRIX_COLS 7
-// wiring of each half
-#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
-#define MATRIX_COL_PINS { D1, D4, C6, D7, E6, B4, B5 }
-#define DIODE_DIRECTION COL2ROW
+#define MASTER_LEFT
+//#define MASTER_RIGHT
 
-// WS2812 RGB LED strip input and number of LEDs
-#define RGB_DI_PIN D3
-#define RGBLED_NUM 12
+//#define EE_HANDS
--- keyboards/handwired/dactyl_manuform/5x7/rules.mk
+++ keyboards/handwired/dactyl_manuform/5x7/rules.mk
@@ -7,10 +7,10 @@ BOOTLOADER = caterina
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+BOOTMAGIC_ENABLE = yes       # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
+CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
